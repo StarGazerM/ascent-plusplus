b36148e4adf72a32fd025255b7b1059111564054
Yihao Sun <stargazermiao@gmail.com>
2024-06-12 17:26:43 -0400

    add example

--- ascent_tests/src/provenance.rs
+++ ascent_tests/src/provenance.rs
@@ -96,22 +96,98 @@ fn test_why_lattice() {
 }
 
 // can we get slog style int autoinc id?
-ascent!{
+#[derive(Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
+struct StructId(&'static str, usize);
+
+ascent_par!{
     struct WhySlog;
 
     relation edge_raw(i32, i32);
     relation edge(i32, i32);
     relation edge_id(i32, i32, usize);
-    relation path(i32, usize);
-    relation path_id(i32, usize, usize);
+    relation path(i32, StructId);
+    relation path_id(i32, StructId, usize);
 
     let eid = !edge(x, y), edge_id(x, y, eid) <--
         edge_raw(x, y);
     
-    let new_id = !path(x, eid), path_id(x, eid, new_id) <--
+    let new_id = !path(x, nest_id.clone()),
+    path_id(x, nest_id, new_id) <--
+        edge_id(x, y, eid),
+        let nest_id = StructId("edge", *eid);
+    
+    let new_id = !path(x, nest_id.clone()),
+    path_id(x, nest_id, new_id) <--
+        edge(x, y),
+        path_id(y, _, pid),
+        let nest_id = StructId("edge", *pid);
+
+}
+
+#[test]
+fn test_why_slog() {
+    let mut tc = WhySlog::default();
+    tc.edge_raw = ascent::boxcar::vec![
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (1, 4),
+    ];
+
+    tc.run();
+
+    println!("path_id: {:?}", tc.path_id);
+    println!("path_id size = {}", tc.path_id.len());
+    // assert_eq!(tc.path_prov.len(), 11);
+}
+
+// where provenance
+
+
+ascent_par! {
+    struct TCWhere;
+
+    relation edge_raw(i32, i32);
+    relation edge(i32, i32);
+    relation edge_id(i32, i32, usize);
+    relation path(i32, i32);
+    relation path_id(i32, i32, usize);
+    relation provenance(StructId, StructId);
+
+    // let <id> = ... will allow you to extract the length of the relation when
+    // head clasue tuple is created. `!` operator will enforce all head clause
+    // to be generated after this clause to be generated after this clause
+    // **succeed** (If a tuple get deduplicated, it means failed).
+    // by combine `let` and `!` in head clauses you can get the slog style autoinc id
+    let eid = !edge(x, y), edge_id(x, y, eid) <--
+        edge_raw(x, y);
+
+    let new_id = !path(x, y),
+    path_id(x, y, new_id),
+    provenance(StructId("path", new_id), StructId("edge", *eid)) <--
         edge_id(x, y, eid);
     
-    let new_id = !path(x, pid), path_id(x, eid, new_id) <--
+    let new_id = !path(x, z),
+    path_id(x, z, new_id),
+    // provenance(StructId("path", new_id), StructId("path", *pid)),
+    provenance(StructId("path", new_id), StructId("edge", *eid)) <--
         edge_id(x, y, eid),
-        path_id(x, eid, pid);
+        path_id(y, z, pid);
+}
+
+#[test]
+fn test_where() {
+    let mut tc = TCWhere::default();
+    tc.edge_raw = ascent::boxcar::vec![
+        (1, 2),
+        (2, 3),
+        (3, 4),
+        (1, 4),
+    ];
+
+    tc.run();
+    println!("provenance size = {}", tc.provenance.len());
+    println!("path_id size = {}", tc.path_id.len());
+    println!("edge_id size = {}", tc.edge_id.len());
+    println!("provenance: {:?}", tc.provenance);
 }
